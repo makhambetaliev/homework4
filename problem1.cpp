59699c58b1270e1ecba8ae915b2d929fc4cf2b1f
makhambetaliev <r.maxambetaliev@newuu.uz>
2024-11-04 23:42:20 +0500

    Update problem1.cpp

--- problem1.cpp
+++ problem1.cpp
@@ -1,25 +1,18 @@
 
 
-//In the comments after each problem functions' names are provided. Your task to define correct datatype and parameters.
-//DO NOT CHANGE FUNCTIONS' NAMES.
-//You may check your functions in main.cpp as shown by exampleFunction;
-
-#include <cmath>
-using namespace std;
-
 void exampleFunction() {
     std::cout<<"Example Function that can be called in main."<<std::endl;
 }
 
 // Problem 1
-// floorFunction
-//ceilingFunction
-int floorFunction(double number) {
-    return floor(number);
+
+template <typename T>
+int floorFunction(T a) {
+    return(int(a));
 }
 
-int ceilingFunction(double number) {
-    return ceil(number);
+template <typename T>
+int ceilingFunction(T a) {
+    return(int(a)+1);
 }
-
 
