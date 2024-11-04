8bbe12bc55d2873caf514d5e85d25afd222e80ea
makhambetaliev <r.maxambetaliev@newuu.uz>
2024-11-04 23:43:04 +0500

    Update problem2.cpp

--- problem2.cpp
+++ problem2.cpp
@@ -1,22 +1,15 @@
 
-
-//In the comments after each problem functions' names are provided. Your task to define correct datatype and parameters.
-//DO NOT CHANGE FUNCTIONS' NAMES.
-//You may check your functions in main.cpp as shown by exampleFunction;
-
-#include <cmath>
-using namespace std;
-
 void exampleFunction() {
     std::cout<<"Example Function that can be called in main."<<std::endl;
 }
 
 
+
 // Problem 2
-//swap_values
-void swap_values(double &a, double &b){
-    a = a+b;
+template <typename T>
+void swap_values(T &a,T &b) {
+    a+=b;
     b = a-b;
-    a = a-b;
+    a-=b;
 }
 
