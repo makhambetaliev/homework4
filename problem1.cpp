77c0bdfba95b0471aa0bbe205dd39e3c12f51971
makhambetaliev <r.maxambetaliev@newuu.uz>
2024-11-04 23:33:28 +0500

    Create problem1.cpp

---
+++ problem1.cpp
@@ -0,0 +1,25 @@
+
+
+//In the comments after each problem functions' names are provided. Your task to define correct datatype and parameters.
+//DO NOT CHANGE FUNCTIONS' NAMES.
+//You may check your functions in main.cpp as shown by exampleFunction;
+
+#include <cmath>
+using namespace std;
+
+void exampleFunction() {
+    std::cout<<"Example Function that can be called in main."<<std::endl;
+}
+
+// Problem 1
+// floorFunction
+//ceilingFunction
+int floorFunction(double number) {
+    return floor(number);
+}
+
+int ceilingFunction(double number) {
+    return ceil(number);
+}
+
+
